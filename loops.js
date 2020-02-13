63b9122e5f0924a10e9daeee273eea14f66668d1
BIT Student <classroom@bgit.rs>
2020-02-13 18:01:19 +0100

    loops

---
+++ loops.js
@@ -0,0 +1,56 @@
+/*for (var number = 10; number > 0; number--) {
+    console.log("Number", number)
+    for (var inner = 0; inner<5; inner++) {
+        console.log("Inner number", inner)
+    }
+}
+
+for(var x=0; x<=10; x++) {
+    console.log(x*x);
+
+}
+
+1.
+
+var a = 0;
+for (a = 0; a <= 15; a++) {
+    if (a % 2 === 0) {
+        console.log("Number\t" + a + "\tis even")
+    } else {
+        console.log("Number\t" + a + "\tis odd")
+    }
+}
+
+
+2.
+var result = 0;
+for (var a = 0; a < 1000; a++) {
+    if (a % 3 === 0 && a % 5 === 0) {
+        result = result + a;
+    }
+}
+console.log(result);
+
+
+3.
+
+var a = [4, 6, 7, 10];
+var sum = 0;
+var product = 1;
+for (var index = 0; index < a.length; index++) {
+    sum = sum + a[index];
+    product = product * a[index];
+}
+console.log(sum, product);
+
+4.
+
+var x = ['1', 'A', 'B', "c", "r", true, NaN, undefined]
+var res = "";
+for (var i = 0; i < x.length; i++) {
+    res = res + x[i];
+}
+console.log(res)*/
+
+
+
