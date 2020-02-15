f390f06ff58ea59ba12e50e0a6f83a18dfba4e50
BIT Student <classroom@bgit.rs>
2020-02-15 17:47:56 +0100

    loops vezbanje

---
+++ stars.js
@@ -0,0 +1,145 @@
+/*var d = 10;
+var row = "";
+var shape = "";
+for (var a = 0; a < d; a++) {
+    row += "*";
+    shape += row + "\n"
+
+
+}
+console.log(shape);
+
+var numbers = [3, 8, "false", 5];
+var copy = [];
+
+for (i = 0; i < numbers.length; i++) {
+    if (typeof numbers[i] === "number") {
+        copy[copy.length] = numbers[i] * 2;
+    }
+}
+console.log(copy);
+
+var numbers = [3, 8, 6];
+var copy = [];
+for (i = 0; i < numbers.length; i++) {
+    if (numbers[i] < 7) {
+        copy[copy.length] = numbers[i];
+    }
+}
+console.log(copy);
+var niz = [3, 8, 7, false, "true", 6];
+var sum = 0;
+var copy = [];
+for (i = 0; i < niz.length; i++) {
+    if (niz[i] <= 6 && typeof niz[i] === "number") {
+        copy[copy.length] = niz[i];
+        sum += niz[i];
+    }
+}
+console.log(copy, "\n", sum);
+
+var first = [3, 4, false];
+var second = [2, 5, 8];
+var combined = [];
+
+for (i = 0; i < first.length; i++) {
+    combined[combined.length] = first[i];
+    combined[combined.length] = second[i];
+}
+console.log(combined);
+
+var first = [3, 4, 5];
+var second = [6, 8, 10];
+var niz = [];
+for (i = 0; i < first.length; i++) {
+    niz[i] = first[i];
+}
+
+for (j = 0; j < second.length; j++) {
+    niz[niz.length] = second[j]
+}
+console.log(niz);
+
+var dimension = 3;
+var square = "";
+for (var i = 0; i < dimension; i++) {
+    var row = "";
+    for (var a = 0; a < dimension; a++) {
+        row += "* ";
+    }
+    square += row + "\n";
+}
+console.log(square);
+1.
+var res = "No";
+var e = 3;
+var a = [5, -4.2, 3, 7];
+for (i = 0; i < a.length; i++) {
+    if (a[i] === e) {
+        res = "Yes";
+    }
+}
+console.log(res);
+
+3.
+var res = "";
+var a = [4, 2, 2, -1, 6];
+var min = a[0];
+var minIndex = 0;
+for (i = 0; i < a.length; i++) {
+    if (min > a[i]) {
+        res = a[i];
+        minIndex = i;
+    }
+}
+console.log(res, minIndex);
+2.
+var a = [-3, 11, 5, 3.4, -8];
+var noviNiz = [];
+
+for (i = 0; i < a.length; i++) {
+    if (a[i] > 0) {
+        noviNiz[i] = a[i] * 2;
+    } else {
+        noviNiz[i] = a[i];
+    }
+}
+console.log(noviNiz);
+
+5.
+var array = [3, 11, -5, -3, 2];
+var sum = 0;
+for (i = 0; i < array.length; i++) {
+    if (array[i] > 0) {
+        sum += array[i];
+    }
+}
+console.log(sum);
+
+9.
+var e = 2;
+var a = [4, 6, 2, 8, 2, 2];
+var niz = [];
+for (i = 0; i < a.length; i++) {
+    if (a[i] !== e) {
+        niz[niz.length] = a[i];
+    }
+}
+console.log(niz);
+10.
+var e = 78;
+var p = 3;
+var a = [2, -2, 33, 12, 5, 8];
+var noviNiz = [];
+for (i = 0; i < a.length; i++) {
+    if (i === p) {
+        noviNiz[noviNiz.length] = e;
+    }
+    noviNiz[noviNiz.length] = a[i];
+}
+console.log(noviNiz);*/
+
+
+
+
+
