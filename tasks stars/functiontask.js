201993f3bb1fa4850a2e1f40503205796aa122b0
BIT Student <classroom@bgit.rs>
2020-02-19 18:03:10 +0100

    funkcije 2

---
+++ tasks stars/functiontask.js
@@ -0,0 +1,90 @@
+//1
+
+// function isString(input) {
+//     if (typeof input === 'string') {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+// var res = isString(12);
+// console.log(res);
+
+//2
+
+// function isBlank(input) {
+//     if (typeof input[i] !== "string") {
+//         return false;
+//     }
+
+//     for (var i = 0; i < input.length; i++) {
+//         if (input[i] !== " ") {
+//             return false;
+//         }
+//     }
+//     return true;
+// }
+// var res = isBlank(false);
+// console.log(res);
+
+
+//3
+
+// function concatenates(str, n) {
+//     var isStr = "";
+//     for (var i = 0; i < n; i++) {
+//         isStr = isStr + str;
+//     }
+//     return isStr;
+// }
+// var res = concatenates('Ha', 5);
+// console.log(res);
+
+//4
+
+// function letterOccurrences(input, n) {
+//     var a = 0;
+//     for (var i = 0; i < input.length; i++) {
+//         if (input[i] === n) {
+//             a++
+//         }
+//     }
+//     return a;
+// }
+
+// var res = letterOccurrences("My random string", "m");
+// console.log(res);
+
+//5
+// function first(str, n) {
+
+//     for (var i = 0; i < str.length; i++) {
+//         if (str[i] === n) {
+//             return i+1
+//         }
+//     }
+//     return -1
+// }
+// var res = first("amajmunii", "i")
+// console.log(res);
+//6
+
+// function first(str, n) {
+
+//     for (var i = str.length; i > 0; i--) {
+//         if (str[i] === n) {
+//             return i + 1
+//             break;
+//         }
+//     }
+//     return -1
+// }
+// var res = first("amajmun        i", "i")
+// console.log(res);
+
+//7
+
+
+
+
+
---
+++ tasks stars/stars.js
@@ -0,0 +1,207 @@
+// var d = 10;
+// var stars = ""
+// for (var i = 0; i < d; i++) {
+//     stars = stars + "*\t";
+//     console.log(stars);
+// }
+
+/** ***************************************************************************/
+
+// var d = 5;
+// var stars = "";
+// var shape = "";
+// for (var i = 0; i < d; i++) {
+//     stars = stars + "* "
+//     shape = shape + stars + "\n"
+
+
+// }
+// console.log(shape);
+
+/********************************************************************** */
+
+// var numbers = [3, 4, 5];
+// var res = "";
+
+// for (var i = 0; i < numbers.length; i++) {
+//     res = res + numbers[i];
+//     res = res + "\n"
+// }
+// console.log(res);
+
+/********************************************************************* */
+
+// var numbers = [3, 4, 5];
+// var copy = [];
+
+// for (var i = 0; i < numbers.length; i++) {
+//     copy[i] = numbers[i]
+// }
+// console.log(copy);
+
+/***************************************************************** */
+
+// var numbers = [3, 4, 5];
+// var copy = [];
+
+// for (var i = 0; i < numbers.length; i++) {
+//     copy[copy.length] = numbers[i];
+// }
+// console.log(copy);
+
+/***********************************Mapiranje*********************** */
+
+// var numbers = [3, 4, 5];
+// var copy = [];
+
+// for (var i = 0; i < numbers.length; i++) {
+//     copy[i] = 2 * numbers[i];
+// }
+// console.log(copy);
+
+/*******************************Mapiranje********************************** */
+
+// var numbers = [3, true, 4, false, 5, 'Stefan'];
+// var copy = [];
+// for (var i = 0; i < numbers.length; i++) {
+//     if (typeof numbers[i] === 'number') {
+//         copy[copy.length] = numbers[i] * 2;
+//     }
+// }
+// console.log(copy);
+
+/*********************Filtriranje******************************************** */
+
+// var number = [3, 8, 9, 4, 7, 5];
+// var modified = [];
+
+// for (var i = 0; i < number.length; i++) {
+//     if (number[i] < 7) {
+//         modified[modified.length] = number[i];
+//     }
+// }
+
+// console.log(modified);
+
+/********************************************************************** */
+
+// var number = [3, 8, 7, false, "true", 6];
+// var sum  = 0;
+
+// for (var i = 0; i <= number.length; i++) {
+//     if (typeof number[i] === 'number' && number[i] <= 6) {
+//         sum = sum + number[i];
+//     }
+// }
+// console.log(sum)
+
+/********************************************************************* */
+
+// var first = [3, 4, false];
+// var second = [2, 5, 8, 10];
+// var res = [];
+// var len;
+
+// if (first.length > second.length) {
+//     len = first.length;
+// } else {
+//     len = second.length;
+// }
+
+// for (var i = 0; i < len; i++) {
+//     if (typeof first[i] !== 'undefined') {
+//         res[res.length] = first[i];
+//     }
+
+//     if (typeof second[i] !== 'undefined') {
+//         res[res.length] = second[i];
+//     }
+// }
+
+// console.log(res)
+
+/********************************************************** */
+
+// var num1 = [3, 4, 5];
+// var num2 = [6, 8, 10];
+// var res = [];
+// for (var i = 0; i < num1.length; i++) {
+//     res[res.length] = num1[i];
+
+// }
+// for (var j = 0; j < num2.length; j++) {
+//     res[res.length] = num2[j];
+// }
+// console.log(res)
+
+/********************************************************* */
+
+// var num1 = [3, 4, 5];
+// var num2 = [6, 8, 10];
+// var res = [];
+// for (var i = 0; i < num1.length; i++) {
+//     res[i] = num1[i];
+//     res[i + num1.length] = num2[i];
+
+// }
+// console.log(res)
+
+/************************************ */
+
+// var d = 3;
+// var row = "";
+// var result = "";
+// for (var i = 0; i < d; i++) {
+//     row = row + "*\t";
+// }
+// for (var j = 0; j < d; j++) {
+//     result = result + row + "\n"
+
+
+
+// }
+// console.log(result)
+
+/***************************************** */
+
+
+
+var d = 4 ;
+var row = "";
+var stars = "";
+var result = "";
+for (var i = 0; i < d; i++) {
+
+
+
+
+
+
+}
+
+
+/******************************************************/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
